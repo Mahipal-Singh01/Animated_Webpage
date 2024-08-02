a323d24636ae986abc18d0d9c687f2abc8e2551f
Mahipal-Singh01 <mahivis0123@gmail.com>
2024-08-02 10:45:25 +0530

    This is my first project

---
+++ Assets/JS/main.js
@@ -0,0 +1,74 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  dots: false,
+  margin: 25,
+  autoplay: true,
+  slideTransition: "linear",
+  autoplayTimeout: 10000,
+  autoplaySpeed: 10000,
+  autoplayHoverPause: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    575: {
+      items: 1,
+    },
+    767: {
+      items: 2,
+    },
+    991: {
+      items: 3,
+    },
+  },
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+                dots: false,
+                items: 3,
+                center: true,
+                margin: 25,
+                autoplay: true,
+                autoplaySpeed: 10000,
+                autoplayHoverPause: false,
+                rtl: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    575: {
+      items: 1,
+    },
+    767: {
+      items: 2,
+    },
+    991: {
+      items: 3,
+    },
+  },
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+                dots: false,
+                items: 3,
+                center: true,
+                margin: 25,
+                autoplay: true,
+                autoplaySpeed: 10000,
+                autoplayHoverPause: false,
+                rtl: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    575: {
+      items: 1,
+    },
+    767: {
+      items: 2,
+    },
+    991: {
+      items: 3,
+    },
+  },
+});
